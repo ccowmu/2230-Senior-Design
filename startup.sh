1a5195bf8397af7ad8ddc23f814c396b7991a214
elainechan01 <elaineyunru.chan@wmich.edu>
2023-01-23 18:09:06 -0500

    bash script init

---
+++ startup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+# install packages
+echo "[STEP 1/3] Downloading python3-serial..."
+sudo apt-get install python3-serial
+if ["$?" -eq "0"]
+then
+	echo "Download successful..."
+else
+	echo "Download failed..."
+fi
+
+# download goodfet
+echo "[STEP 2/3] Downloading goodfet..."
+git clone https://github.com/travisgoodspeed/goodfet goodfet
+if ["$?" -eq "0"]
+then
+        echo "Download successful..."
+else
+        echo "Download failed..."
+fi
+
+# link client scripts
+echo "[STEP 3/3] Linking client scripts...
+cd goodfet/client && sudo make link
+if ["$?" -eq "0"]
+then
+        echo "Download successful..."
+else
+        echo "Download failed..."
+fi
+
+# store env var
+echo "export board=goodfet41" >> ~/.bashrc
+
